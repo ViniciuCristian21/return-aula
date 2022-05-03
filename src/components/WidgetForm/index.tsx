63249dc0a61e706191de3f15f72fd7db1a8109a2
Viniciu Cristian <git.viniciu@gmail.com>
2022-05-03 16:59:04 -0300

    Aula 02

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,69 @@
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/ideia.svg';
+import otherImageUrl from '../../assets/outro.svg';
+import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackSuccessStep } from './Steps/FeedbackSuccessStep';
+import { FeedbackContentStep } from './Steps/FeedbackContentStep';
+
+
+export const feedbackTypes = {
+    BUG: {
+        title: 'Problema',
+        image: {
+            source: bugImageUrl,
+            alt: 'Imagem de um inseto'
+        }
+    },
+    IDEA: {
+        title: 'Ideia',
+        image: {
+            source: ideaImageUrl,
+            alt: 'Imagem de uma lampada'
+        }
+    },
+    OTHER: {
+        title: 'Outro',
+        image: {
+            source: otherImageUrl,
+            alt: 'Imagem de um balão de pensamento'
+        }
+    },
+}
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
+    const [feedbackSent, setFeedbackSent] =  useState(false)
+
+    function handleRestartFeedback() {
+        setFeedbackSent(false)
+        setFeedbackType(null);
+    }
+
+    return(
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+            {feedbackSent ?(
+                <FeedbackSuccessStep onFeedbackRestartRequested={handleRestartFeedback}/>
+            ): (
+                <>
+                    {!feedbackType ? (
+                        <FeedbackTypeStep onFeedbackTypeChange={setFeedbackType} />
+                    ): (
+                        <FeedbackContentStep
+                        feedbackType={feedbackType}
+                        onFeedbackRestartRequested={handleRestartFeedback}
+                        onFeedbackSent={() => setFeedbackSent(true)}
+                        />
+                    )}
+                </>
+            )}
+
+            <footer>
+                Feito com ❤️ pela <a href="#" className="underline underline-offset-2">Rocketseat</a>
+            </footer>
+        </div>
+    )
+}
